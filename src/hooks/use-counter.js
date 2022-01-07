13f3331fbcf84fc7891b1f27f5e68172a074e075
Master1624 <jdpa83@gmail.com>
2022-01-07 11:21:17 -0500

    Final Commit

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
 
 // To create your own hook, the beginning of the func name should ALWAYS start with use
-const useCounter = () => {
+const useCounter = (forwards = true) => {
 	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCounter((prevCounter) => prevCounter + 1);
+			if (forwards) {
+				setCounter((prevCounter) => prevCounter + 1);
+			}
+      else{
+        setCounter((prevCounter) => prevCounter - 1);
+      }
 		}, 1000);
 
 		return () => clearInterval(interval);
-	}, []);
+	}, [forwards]);
 
 	return counter;
 };
